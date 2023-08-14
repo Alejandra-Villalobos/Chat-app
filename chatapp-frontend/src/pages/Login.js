aed5db8411410f118d8607047bd351631586c1f4
alejandra-villalobos <00132820@uca.edu.sv>
2023-08-13 19:33:57 -0600

    Added: Login interface

---
+++ chatapp-frontend/src/pages/Login.js
@@ -0,0 +1,41 @@
+import { FcGoogle } from "react-icons/fc";
+import chatImage from "../assets/chat_image.png"
+
+function Login() {
+  return (
+    <div className="flex font-patua">
+      <div className="w-1/2 bg-emerald-200 h-screen flex flex-col justify-center items-center gap-4">
+        <p className="text-6xl">ChatApp</p>
+        <button className="my-4 mx-2 flex gap-2 items-center text-2xl text-gray-600 bg-white rounded-md px-6 py-3 w-1/2 hover:bg-gray-100">
+          <FcGoogle size={25} />
+          Log in with Google Accounts
+        </button>
+        <div className="flex items-center justify-center w-1/2 gap-2">
+          <hr className="border border-gray-500 mt-1 w-full rounded-lg" />
+          <p className="text-gray-600">or</p>
+          <hr className="border border-gray-500 mt-1 w-full rounded-lg" />
+        </div>
+        <form className="flex flex-col w-1/2">
+          <input
+            className="mt-4 flex gap-5 text-xl text-gray-700 bg-white rounded-md px-6 py-3 w-full"
+            placeholder="Username"
+          ></input>
+          <input
+            className="mt-4 flex gap-5 text-xl text-gray-700 bg-white rounded-md px-6 py-3 w-full"
+            placeholder="Password"
+          ></input>
+          <input
+            className="mt-4 flex gap-5 text-2xl text-white bg-fuchsia-300 rounded-md px-6 py-3 w-full hover:bg-fuchsia-400 cursor-pointer"
+            type="submit"
+            value="Log in"
+          ></input>
+        </form>
+      </div>
+      <div className="w-1/2 bg-yellow-100 h-screen">
+        <img alt="Chat" src={chatImage} className="w-full h-full"/>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
