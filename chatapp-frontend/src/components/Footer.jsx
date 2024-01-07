97498fd7b43f90f39e756045453d36df62f41ad3
alejandra-villalobos <00132820@uca.edu.sv>
2024-01-06 21:14:28 -0600

    🚀 Improved: Dockerfile in frontend

--- chatapp-frontend/src/components/Footer.jsx
+++ chatapp-frontend/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 function Footer() {
   return (
     <div className='w-screen h-max p-2 flex justify-center gap-2 bg-teal-800 absolute bottom-0 z-20'>
-        <FaGithub size={25} color='white' href='https://github.com/Alejandra-Villalobos/'/>
-        <FaLinkedin size={25} color='white' href='https://www.linkedin.com/in/alejandra-villalobos-558972203/'/>
+        <a href='https://github.com/Alejandra-Villalobos/' target="_blank" rel="noopener noreferrer"><FaGithub size={25} color='white'/></a>
+        <a href='https://www.linkedin.com/in/alejandra-villalobos-558972203/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={25} color='white'/></a>
     </div>
   )
 }
